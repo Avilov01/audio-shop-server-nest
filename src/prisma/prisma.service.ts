59795056578caeaaf60128558ba085a16913f73c
d.avilov <avilov01@gmail.com>
2026-05-31 18:40:28 +0300

    fix run project

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,25 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '../../generated/prisma/client';
 
+function createPrismaAdapter() {
+  const connectionString = process.env.DATABASE_URL;
+
+  if (!connectionString) {
+    throw new Error('DATABASE_URL is not set');
+  }
+
+  return new PrismaPg({ connectionString });
+}
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      adapter: createPrismaAdapter(),
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
